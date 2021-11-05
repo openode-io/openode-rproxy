b7a0d6f4088e614bf5f156db9bf0c182f9ce9274
Martin Lévesque <levesque.martin@gmail.com>
2021-11-04 20:20:52 -0400

    E2e (#2)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip

---
+++ test/openode_api.js
@@ -0,0 +1,25 @@
+
+const http = require("http");
+
+const port = 3000
+console.log(`Port is ${port}`)
+
+const server = http.createServer(function (req, res) {
+    console.log(`Request on ${req.url}`)
+    
+    if (req.url === '/super_admin/website_locations/load_balancer_requiring_sync') {
+        res.end(
+            `[{"hosts": ["mytest.openode.io"], "id": 10, "website_id": 999, "backend_url": "https://www.openode.io/"}]`
+        );
+    }
+    else if (req.url === '/super_admin/website_locations/10') {
+        res.end(`{}`);   
+    }
+    else {
+        res.end(`!!Hello opeNode World`);
+    }
+}).listen(port, (err) => {
+    if ( ! err) {
+        console.log(`node server listening on port ${port}...`, new Date())
+    }
+})
