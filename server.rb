e9727fc416cce5f9fc93772edfe41ab5ada145d6
Martin Lévesque <levesque.martin@gmail.com>
2021-11-11 21:21:08 -0500

    sync cleanup remove inactive configs (#14)
    * sync cleanup remove inactive configs
    
    * fix

--- server.rb
+++ server.rb
@@ -104,6 +104,13 @@ end
 
 log("Booting with sync interval #{LOOP_SYNC_INTERVAL}")
 
+def config_filename(website_location)
+  website_id = website_location["website_id"]
+  website_location_id = website_location["id"]
+
+  "#{website_id}-#{website_location_id}.yml"
+end
+
 def sync_website_locations(website_locations, with_set_load_balancer_synced = false)
   website_locations.each do |website_location|
     wl = website_location
@@ -115,18 +122,34 @@ def sync_website_locations(website_locations, with_set_load_balancer_synced = fa
 
     data = engine.render
 
-    website_id = wl["website_id"]
     website_location_id = wl["id"]
-    file_path = "#{CONFIGS_PATH}/#{website_id}-#{website_location_id}.yml"
+    file_path = "#{CONFIGS_PATH}/#{config_filename(wl)}"
     File.open(file_path, 'w') { |file| file.write(data) }
-    log("[+] Wrote #{file_path}")
+    log "[+] Wrote #{file_path}"
 
     openode_set_load_balancer_synced(website_location_id) if with_set_load_balancer_synced
   end
 end
 
+def sync_clean_inactive_website_locations(website_locations)
+  config_filenames = Dir.entries(CONFIGS_PATH)
+
+  website_location_filenames = website_locations.map { |wl| config_filename(wl) }
+  filenames_ok = ["http-catchall.yml", ".", ".."] + website_location_filenames
+
+  filenames_to_remove = config_filenames - filenames_ok
+
+  filenames_to_remove.each do |filename_to_remove|
+    filepath = "#{CONFIGS_PATH}/#{filename_to_remove}"
+    log "Removing file #{filepath}"
+
+    File.delete(filepath)
+  end
+end
+
 # On boot, do a global sync
 initial_website_locations = openode_all_sites_online_gcloud_run
+sync_clean_inactive_website_locations(initial_website_locations)
 sync_website_locations(initial_website_locations, false)
 initial_website_locations = nil
 
