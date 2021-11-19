f0c4c772ce2e89b4c98a6ed20c438d6ae3be24be
Martin Levesque <levesque.martin@gmail.com>
2021-11-19 15:37:44 -0500

    fix test

--- test/openode_api.js
+++ test/openode_api.js
@@ -7,12 +7,12 @@ console.log(`Port is ${port}`)
 const server = http.createServer(function (req, res) {
     console.log(`Request on ${req.url}`)
     
-    if (req.url === '/super_admin/website_locations/load_balancer_requiring_sync') {
+    if (req.url === '/super_admin/website_locations/load_balancer_requiring_sync?location=us-central-1') {
         res.end(
             `[{"hosts": ["mytest.openode.io"], "id": 10, "website_id": 999, "backend_url": "https://www.openode.io/"}]`
         );
     }
-    else if (req.url === '/super_admin/website_locations/online/gcloud_run') {
+    else if (req.url === '/super_admin/website_locations/online/gcloud_run?location=us-central-1') {
         res.end(
             `[{"hosts": ["myboottest.openode.io"], "id": 11, "website_id": 1000, "backend_url": "https://api.openode.io/"}]`
         );
